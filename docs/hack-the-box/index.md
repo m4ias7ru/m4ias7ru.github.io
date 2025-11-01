aeabd6a830ca0c566fdfe6999ea9820aef0156e0
Marius-Alexandru Ulmeanu <ulmeanu.m.cybersec@gmail.com>
2025-11-01 08:45:17 +0200

    Remove default content and set docs as homepage

---
+++ docs/hack-the-box/index.md
@@ -0,0 +1,28 @@
+---
+slug: /
+title: m4ias7ru
+sidebar_label: Home
+---
+
+# `m4ias7ru:~$`
+
+<!-- This site is an archive of my Hack The Box writeups and cybersecurity notes.
+
+## Categories
+
+Use the sidebar to navigate the full file tree or select a main category below.
+
+### [Active Directory Exploitation](./active-directory-exploitation)
+> Writeups focused on Active Directory, Kerberos, and Windows domain compromise.
+
+### [Detecting Active Directory Attacks](./detecting-active-directory-attacks)
+> Writeups focused on Detecting Active Directory Attacks Sherlocks.
+
+### [Linux Machines](./linux)
+> *(Coming Soon)* Standard Linux box writeups.
+
+### [Windows Machines](./windows)
+> *(Coming Soon)* Non-AD Windows box writeups.
+
+### [Cheatsheets](./cheatsheets)
+> *(Coming Soon)* Quick-reference guides and command snippets. -->
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,9 +10,9 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: 'm4ias7ru',
+  tagline: 'Cybersecurity',
+  favicon: 'img/logo.svg',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -47,10 +47,8 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // Make this the default homepage
+          routeBasePath: '/',
         },
         blog: {
           showReadingTime: true,
@@ -58,10 +56,6 @@ const config = {
             type: ['rss', 'atom'],
             xslt: true,
           },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
@@ -78,75 +72,35 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/social-card.png',
       colorMode: {
         respectPrefersColorScheme: true,
       },
       navbar: {
-        title: 'My Site',
+        title: 'm4ias7ru',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'm4ias7ru',
           src: 'img/logo.svg',
         },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
+        // items: [
+        //   {
+        //   }
+        // ],
       },
       footer: {
         style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'X',
-                href: 'https://x.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        // links: [
+        //   {
+            
+        //   },
+        //   {
+        //     title: 'More',
+        //     items: [
+
+        //     ],
+        //   },
+        // ],
+        copyright: `Copyright © ${new Date().getFullYear()} Marius-Alexandru Ulmeanu`,
       },
       prism: {
         theme: prismThemes.github,
